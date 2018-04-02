5d2ebccce29e52530d7ca9bc473d6e0fd7a0ca57
Nikita Ryaskin <nik.ryaskin@gmail.com>
2018-04-02 20:26:13 +0300

    Adding matrix transformations

--- transformation3d.cpp
+++ transformation3d.cpp
@@ -6,22 +6,40 @@ void Transformation3D::translate(const QVector3D &dt)
     m_translation += dt;
 }
 
-void Transformation3D::setTranslation(const QVector3D &t)
-{
-    m_dirty = true;
-    m_translation = t;
-}
-
 void Transformation3D::rotate(const QQuaternion &dr)
 {
     m_dirty = true;
     m_rotation *= dr;
 }
 
+void Transformation3D::scale(const QVector3D &ds)
+{
+   m_dirty = true;
+   m_scale *= ds;
+}
+
+void Transformation3D::grow(const QVector3D &ds)
+{
+    m_dirty = true;
+    m_scale += ds;
+}
+
+void Transformation3D::setTranslation(const QVector3D &t)
+{
+    m_dirty = true;
+    m_translation = t;
+}
+
 void Transformation3D::setRotation(const QQuaternion &dr)
 {
     m_dirty = true;
     m_rotation = dr;
+}
+
+void Transformation3D::setScale(const QVector3D &s)
+{
+    m_dirty = true;
+    m_scale = s;
 }
 
 const QMatrix4x4& Transformation3D::toMatrix()
--- transformation3d.h
+++ transformation3d.h
@@ -16,6 +16,12 @@ public:
     void rotate(const QQuaternion &dr);
     void rotate(float angle, const QVector3D &axis);
     void rotate (float angle, float ax, float ay, float az);
+    void scale(const QVector3D &ds);
+    void scale(float dx, float dy, float dz);
+    void scale(float factor);
+    void grow(const QVector3D &ds);
+    void grow(float dx, float dy, float dz);
+    void grow(float factor);
 
     // Transform To (Setters)
     void setTranslation(const QVector3D &t);
@@ -23,15 +29,20 @@ public:
     void setRotation(const QQuaternion &dr);
     void setRotation(float angle, const QVector3D &axis);
     void setRotation(float angle, float ax, float ay, float az);
+    void setScale(const QVector3D &s);
+    void setScale(float x, float y, float z);
+    void setScale(float k);
 
 
     // Accessors
     const QVector3D& translation() const;
     const QQuaternion& rotation() const;
+    const QVector3D& scale() const;
     const QMatrix4x4& toMatrix();
 private:
     bool m_dirty;
     QVector3D m_translation;
+    QVector3D m_scale;
     QQuaternion m_rotation;
     QMatrix4x4 m_world;
 };
@@ -42,13 +53,20 @@ inline Transformation3D::Transformation3D() : m_dirty(true) {}
 inline void Transformation3D::translate(float dx, float dy,float dz) { translate(QVector3D(dx, dy, dz)); }
 inline void Transformation3D::rotate(float angle, const QVector3D &axis) { rotate(QQuaternion::fromAxisAndAngle(axis, angle)); }
 inline void Transformation3D::rotate(float angle, float ax, float ay,float az) { rotate(QQuaternion::fromAxisAndAngle(ax, ay, az, angle)); }
+inline void Transformation3D::scale(float dx, float dy,float dz) { scale(QVector3D(dx, dy, dz)); }
+inline void Transformation3D::scale(float factor) { scale(QVector3D(factor, factor, factor)); }
+inline void Transformation3D::grow(float dx, float dy, float dz) { grow(QVector3D(dx, dy, dz)); }
+inline void Transformation3D::grow(float factor) { grow(QVector3D(factor, factor, factor)); }
 
 // Transform To (Setters)
 inline void Transformation3D::setTranslation(float x, float y, float z) { setTranslation(QVector3D(x, y, z)); }
 inline void Transformation3D::setRotation(float angle, const QVector3D &axis) { setRotation(QQuaternion::fromAxisAndAngle(axis, angle)); }
 inline void Transformation3D::setRotation(float angle, float ax, float ay, float az) { setRotation(QQuaternion::fromAxisAndAngle(ax, ay, az, angle)); }
+inline void Transformation3D::setScale(float x, float y, float z) { setScale(QVector3D(x, y, z)); }
+inline void Transformation3D::setScale(float k) { setScale(QVector3D(k, k, k)); }
 
 // Accessors
 inline const QVector3D& Transformation3D::translation() const { return m_translation; }
 inline const QQuaternion& Transformation3D::rotation() const { return m_rotation; }
+inline const QVector3D& Transformation3D::scale() const { return m_scale; }
 #endif // TRANSFORMATION3D_H
