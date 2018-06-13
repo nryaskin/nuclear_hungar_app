4bd89752e2fd0ff4191294fa52c8c31024bb1545
Nikita Ryaskin <nik.ryaskin@gmail.com>
2018-06-13 08:56:07 +0300

    Add table for interaction

--- src/view/mainwindowwidget.cpp
+++ src/view/mainwindowwidget.cpp
@@ -1,20 +1,22 @@
 #include "mainwindowwidget.h"
-
+#include <QTableWidgetItem>
 MainWindowWidget::MainWindowWidget(QWidget *parent) : QWidget(parent)
 {
     HangarView *hg_view = new HangarView();
     QGridLayout *layout = new QGridLayout;
     QWidgetItem* hangar_item = new QWidgetItem(hg_view);
-    layout->addItem(hangar_item, 0, 0, 2, 3);
+    layout->addItem(hangar_item, 0, 0, 4, 35);
 
-    QPushButton *button1 = new QPushButton("One");
-    QPushButton *button2 = new QPushButton("Two");
-    QPushButton *button3 = new QPushButton("Three");
+    QPushButton *button1 = new QPushButton("Добавить ящик");
+    QPushButton *button2 = new QPushButton("Убрать ящик");
+    QPushButton *button3 = new QPushButton("Построить маршрут");
 
     QVBoxLayout *ux_layout = new QVBoxLayout;
+    QTableWidget* tableWidget = new QTableWidget(4, 14);
     ux_layout->addWidget(button1);
+    ux_layout->addWidget(tableWidget);
     ux_layout->addWidget(button2);
     ux_layout->addWidget(button3);
-    layout->addItem(ux_layout,0, 3, 2, 1);
+    layout->addItem(ux_layout,0, 35, 4, 14);
     setLayout(layout);
 }
